3edd7e812bfa1138795e129a75c8249e36dca3b4
AlokRanjan-github <alokrp7007@gmail.com>
2024-08-12 11:39:12 +0530

    heroDone

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-const Button = ({ label, iconURL }) => {
+
+export const Button = ({ label, iconURL }) => {
     return (
         <button className='flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red'>
             {label}
@@ -9,10 +10,6 @@ const Button = ({ label, iconURL }) => {
                 alt="arrow right icon"
                 className='ml-2 rounded-full w-5 h-5 '
             />
-
-
         </button>
     )
 }
-
-export default Button
