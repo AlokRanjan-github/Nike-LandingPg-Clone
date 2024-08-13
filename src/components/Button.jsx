7a9e036452aa0e8583550825e82a39830d1ab6ac
AlokRanjan-github <alokrp7007@gmail.com>
2024-08-13 12:58:02 +0530

    section_7 subs

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 
 
-export const Button = ({ label, iconURL, backgroundColor, borderColor, textColor }) => {
+export const  Button = ({ label, iconURL, backgroundColor, borderColor, textColor, fullWidth }) => {
     return (
-        <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none 
-        ${backgroundColor
-                ? `${backgroundColor} ${textColor} ${textColor}`
-                : ' bg-coral-red  text-white border-coral-red '
-        } rounded-full `}>
+        <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+      ${
+        backgroundColor
+          ? `${backgroundColor} ${textColor} ${borderColor}`
+          : "bg-coral-red text-white border-coral-red"
+      } rounded-full ${fullWidth && "w-full"}`}
+    >
             {label}
             {iconURL && <img
                 src={iconURL}
