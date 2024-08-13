23e44852efbcdc1f18105d85ede1d5d2f484a286
AlokRanjan-github <alokrp7007@gmail.com>
2024-08-13 10:33:43 +0530

    section_4 services

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -11,7 +11,7 @@ const SuperQuality = () => {
       <div className='flex flex-1 flex-col '>
         <h2 className='font-palanquin text-4xl capitalize font-bold lg:max-w-lg'>
           We Provide You
-          <span className='text-coral-red'>Super </span>
+          <span className='text-coral-red'> Super </span>
           <span className='text-coral-red'>Quality </span> Shoes
         </h2>
         <p className='mt-4 lg:max-w-lg info-text'>Ensuring premium comfort and style, our meticulously crafted footwear is designed to elevate your experience, providing you with unmatched quality, innovation, and a touch of elegance.</p>
